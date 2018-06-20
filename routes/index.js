6b9d8edb7ca170e8c6d67f99c6a942e6d4ea1337
yongchaekim <yongchaekim94@gmail.com>
2018-06-21 01:56:28 +0900

    added magic

--- routes/index.js
+++ routes/index.js
@@ -136,9 +136,7 @@ router.get('/login', function(req, res, next){
 router.get('/emotion', function(req, res, next){
     return res.render('emotion.ejs');
 });
-router.get('/speech', function(req, res, next){
-    return res.render('speech.ejs');
-});
+
 
 router.post('/login', function(req, res, next){
     User.findOne({email:req.body.email}, function(err, data){
